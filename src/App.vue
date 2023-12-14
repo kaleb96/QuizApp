738857db46ff299f57b04722953139efd221d714
kaleb96 <yes505304@gmail.com>
2023-12-14 15:13:27 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <RouterView/>
+  </div>
+</template>
+
+
+
+<style scoped>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+</style>
