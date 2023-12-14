d04fb7853e1dfc4c2b0a20ca400cecd6dceb2398
kaleb96 <yes505304@gmail.com>
2023-12-14 18:23:26 +0900

    #1
    - [x] Quiz Page HTML and CSS
    - [x] Separating Code into Components
    - [x] Passing Props to the Question
    - [x] Listening to Changes with Watch
    - [x] Computing Values With State

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const { questionStatus } = defineProps(['questionStatus'])
+</script>
+
+<template>
+    <header>
+        <h4>{{ questionStatus }}</h4>
+        <div class="bar">
+            <div class="completion"></div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+
+header {
+        margin-top: 20px
+    }
+
+    header h4 {
+        font-size: 30px;
+    }
+
+    .bar {
+        width: 300px;
+        height: 50px;
+        border: 3px solid bisque 
+    }
+
+    .completion {
+        height: 100%;
+        width: 0%;
+        background-color: bisque;
+    }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,13 +1,41 @@
-<template>
-    <div>Hello</div>
-</template>
+<script setup>
+    import Question from '../components/Question.vue'
+    import QuizHeader from '../components/QuizHeader.vue'
+    import { useRoute } from 'vue-router';
+    import { ref, watch, computed} from 'vue'
+    import quizes from '../data/quizes.json'
 
-<script>
-export default {
 
-}
+    const route = useRoute()
+    const quizId = parseInt(route.params.id);
+    const currentQuestionIndex = ref(0);
+    const quiz = quizes.find((q => q.id === quizId))
+
+
+
+    /*Watch를 통해서 currentQuestionIndex를 감시하여 update하는 방법 
+    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+    watch(() => currentQuestionIndex.value, () => {
+        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+    })
+    */
+   
+    /* computed를 통해서 currertQuestionIndex를 update하는 방법 
+    const questionStatus =  computed(() => {   
+        return `${currentQuestionIndex.value}/${quiz.questions.length}`
+    })
+    */
+    const questionStatus =  computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+
 </script>
 
-<style>
-
-</style>
+<template>
+    <div>
+        <quiz-header :questionStatus="questionStatus"/>
+        <div>
+            <question :question="quiz.questions[currentQuestionIndex]"/>
+        </div>
+        <button @click="currentQuestionIndex++">Next Question</button>
+    </div> <!-- main div-->
+</template>
+  components: { QuizHeader },
